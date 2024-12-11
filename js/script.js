107b6fe05e81cc0d94ce775986c7b57961732408
Əyyub Əkbərov <yyubekber4@gmail.com>
2024-12-11 20:18:59 +0400

    Event

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function messi() {
+    document.getElementById("user").src = "img/messi.webp"
+}
+function ronol() {
+    document.getElementById("user").src = "img/ronaldo_orig.gif"
+}
+function n() {
+    document.getElementById("user").src = "img/neymar.gif"
+}
+function l() {
+    document.getElementById("user").src = "img/giphy2.gif"
+}
+function m() {
+    document.getElementById("user").src = "img/giphy.webp"
+}
+function b() {
+    document.getElementById("user").src = "img/karim-benzema-magique.gif"
+}
+function out() {
+    document.getElementById("user").src = "img/question-mark-poster.png"
+}
